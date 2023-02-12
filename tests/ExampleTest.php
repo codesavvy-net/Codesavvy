69dde45c490625707b3f4ba0d32c0a1b3a4eb8fa
Nícola Serafim <nicola@seraf.im>
2023-02-12 04:13:50 -0300

    feat: Adiciona estrutura para testes

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+  public function testExample()
+  {
+
+    $test = null;
+
+    $this->assertEmpty($test);
+  }
+}
