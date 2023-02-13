b80ffb1cf3443624f839e4c6b614301b2d957aae
MatheusBrandao525 <mafe123silva@gmail.com>
2023-02-13 00:19:27 -0300

    feat: Criação do metodo para deletar registros do banco de dados

--- src/Utils/Connection.php
+++ src/Utils/Connection.php
@@ -73,23 +73,50 @@ use PDOException;
     $seters .= "{$key}=':{$key}',";
    }
 
-
+   // A string $seters é cortada para remover a última vírgula.
    $seters = substr($seters, 0, -1);
+
+   // A variável $altera é preenchida com uma instrução SQL que irá atualizar a tabela self::$table com os valores definidos em $seters e onde a coluna específica (definida por $column) é igual ao valor específico (definido por $value).
     $altera = self::$conn->prepare("UPDATE ".self::$table." SET {$seters}  WHERE {$column} = :value");
 
+    // O método bindParam é usado para ligar o valor de $value ao placeholder :value na instrução SQL
    $altera->bindParam(':value', $value);
 
+   // Outro loop foreach é usado para iterar sobre o array $columns e ligar cada valor ao seu respectivo placeholder na instrução SQL.
    foreach($columns as $key => $item){
     $altera->bindParam(":{$key}", $item);
    }
+
+      // O método execute é usado para executar a instrução SQL.
      $altera->execute();
 
-     // Se encontrar um linha na tabela, retorna um array com os valores
+     // Se a consulta encontrar uma linha na tabela, ela retornará um array com os valores da coluna específica (obtido pelo método getColumn)
      if($altera->rowCount())
         return self::getColumn($column, $value);
 
-      // Se não, retorna um valor null
+      // Se a consulta não encontrar nenhuma linha na tabela, ela retornará o valor null.
      return null;
+
+  }
+
+
+   /**
+    * Este é um método que exclui uma linha específica da tabela. Ele tem dois parâmetros: $columne $value, que especificam a coluna e o valor para deletar respectivamente.
+    * @param $column string enviar o nome da coluna especifica para ser deletada
+    * @param $value string passar o valor especifico da coluna que deseja deletar
+    * @return bool
+   */
+  static function deleteColumn(string $column, string $value):bool
+  {
+      //Essa linha usa o objeto de $conn conexão para preparar uma consulta SQL para deletar uma linha da tabela self::$table
+     $delete = self::$conn->prepare("DELETE '{$column}' FROM ".self::$table." WHERE {$value} = :value");
+     $delete->bindParam(':value', $value);
+
+     //// O método execute é usado para executar a instrução SQL.
+     $delete->execute();
+
+     // Verifica se a coluna foi deletada, se sim retorna true, se não retorna false.
+     return (bool) $delete->rowCount();
 
   }
    
