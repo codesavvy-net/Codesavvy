c818b5537f05348d7c2d565bd9e7238f746f1550
Nícola Serafim <nicola@seraf.im>
2023-02-13 02:28:53 -0300

    chore: Preparar o ambiente para conexão do banco de dados, proteção csrf

--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
-<?php
-
-use Slim\Factory\AppFactory;
+<?php ob_start();
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
-
 require __DIR__ . DS . '..' . DS . 'src' . DS . 'Http' . DS . 'bootstrap.php';
-
-$app->run();
--- src/Http/bootstrap.php
+++ src/Http/bootstrap.php
@@ -1,9 +1,21 @@
 <?php
 
 use Esquadrao21\Squad21\Utils\Env;
+use DI\Container;
+use Slim\Factory\AppFactory;
+
+session_start();
 
 //Carrega Variáveis de ambiente do arquivo .env
 Env::load();
+
+$container = new Container();
+AppFactory::setContainer($container);
+
+// Create App
+$app = AppFactory::create();
+
+$responseFactory = $app->getResponseFactory();
 
 //Verifica se a aplicação esta em produção
 if ($_ENV['ENVIRONMENT'] !== 'production') {
@@ -23,3 +35,8 @@ require "Twig.php";
 
 //Adiciona as Rotas
 require 'Routes.php';
+
+//Adiciona CSRF
+require 'Csrf.php';
+
+$app->run();
